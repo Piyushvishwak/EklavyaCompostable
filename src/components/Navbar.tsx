f8c8014e1378326368924fd5be06b9cdddf9dfd9
Piyush Vishwakarma <115068496+Piyushvishwak@users.noreply.github.com>
2025-07-08 17:29:22 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        isScrolled ? 'bg-blck shadow-md py-3' : 'bg-black py-5'
+        isScrolled ? 'bg-black shadow-md py-3' : 'bg-black py-5'
       }`}
     >
       <div className="container mx-auto px-4 md:px-8">
